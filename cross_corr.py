eb80e1ef27f3b01fb2a22b47c66ea02ba970eb5e
Kristen Surrao <35614246+kmsurrao@users.noreply.github.com>
2024-01-31 23:58:23 -0500

    initial modifications for map level difference

--- cross_corr.py
+++ cross_corr.py
@@ -42,7 +42,7 @@ def randsphere(num, ra_range=[0,360], dec_range=[-90,90]):
 
 
 
-def compute_chi2_real_space(inp, y1, y2, ra_halos, dec_halos, beta, include_covhy2=True):
+def compute_chi2_real_space(inp, y1, y2, ra_halos, dec_halos, beta):
     '''
     ARGUMENTS
     ---------
@@ -53,17 +53,13 @@ def compute_chi2_real_space(inp, y1, y2, ra_halos, dec_halos, beta, include_covh
     ra_halos: ndarray containing ra of halos
     dec_halos: ndarray containing dec of halos
     beta: float, beta value used for CIB deprojection (just used for file naming here)
-    include_covhy2: Bool, whether to include covariance of h x y2 in chi2 computation
 
     RETURNS
     -------
     chi2: float, chi^2 value of <h,y1> and <h,y2>
-    xi_hy1: 1D numpy array of length nrad containing correlation of halos with y1
-    xi_hy2: 1D numpy array of length nrad containing correlation of halos with y2
-    cov_hy1: 3D numpy array of shape (2,2,nrad) containing covariance 
-        matrix of halos and y1
-    cov_hy2: 3D numpy array of shape (2,2,nrad) containing covariance 
-        matrix of halos and y2
+    xi_hy: 1D numpy array of length nrad containing correlation of halos with (y1-y2)
+    cov_hy: 2D numpy array of shape (nrad, nrad) containing covariance 
+        matrix of halos and (y1-y2)
     r_hy: 1D numpy array of length nrad containing x-axis point for plotting
     '''
     rand_ra, rand_dec = randsphere(5*len(ra_halos), ra_range=[0,360], dec_range=[-90,90])
@@ -93,42 +89,28 @@ def compute_chi2_real_space(inp, y1, y2, ra_halos, dec_halos, beta, include_covh
                                         dec_units='degrees', npatch=njk)
         datapoint_cat.write_patch_centers(save_dir + save_filename_jk_obj)
     
-    # Create random catalog and catalogs for y1 and y2
+    # Create random catalog and catalog for (y1-y2)
     rand_cat = treecorr.Catalog(ra=rand_ra, dec=rand_dec, ra_units='degrees', dec_units='degrees', patch_centers=save_dir + save_filename_jk_obj)
-    y1_cat = treecorr.Catalog(ra=ra_hp, dec=dec_hp, k=y1, ra_units='degrees', dec_units='degrees')
-    y2_cat = treecorr.Catalog(ra=ra_hp, dec=dec_hp, k=y2, ra_units='degrees', dec_units='degrees')
+    y_cat = treecorr.Catalog(ra=ra_hp, dec=dec_hp, k=(y1-y2), ra_units='degrees', dec_units='degrees')
 
     # create the correlation objects:
-    hy1 = treecorr.NKCorrelation(nbins=nrad, min_sep=minrad, max_sep=maxrad, sep_units='arcmin', verbose=0, num_threads=nthreads, bin_slop=bin_slop, var_method='jackknife')
-    hy2 = treecorr.NKCorrelation(nbins=nrad, min_sep=minrad, max_sep=maxrad, sep_units='arcmin', verbose=0, num_threads=nthreads, bin_slop=bin_slop, var_method='jackknife')
-    ry1 = treecorr.NKCorrelation(nbins=nrad, min_sep=minrad, max_sep=maxrad, sep_units='arcmin', verbose=0, num_threads=nthreads, bin_slop=bin_slop, var_method='jackknife')
-    ry2 = treecorr.NKCorrelation(nbins=nrad, min_sep=minrad, max_sep=maxrad, sep_units='arcmin', verbose=0, num_threads=nthreads, bin_slop=bin_slop, var_method='jackknife')
+    hy = treecorr.NKCorrelation(nbins=nrad, min_sep=minrad, max_sep=maxrad, sep_units='arcmin', verbose=0, num_threads=nthreads, bin_slop=bin_slop, var_method='jackknife')
+    ry = treecorr.NKCorrelation(nbins=nrad, min_sep=minrad, max_sep=maxrad, sep_units='arcmin', verbose=0, num_threads=nthreads, bin_slop=bin_slop, var_method='jackknife')
 
-    hy1.process(datapoint_cat, y1_cat)
-    hy2.process(datapoint_cat, y2_cat)
-    ry1.process(rand_cat, y1_cat)
-    ry2.process(rand_cat, y2_cat)
+    hy.process(datapoint_cat, y_cat)
+    ry.process(rand_cat, y_cat)
 
-    hy1.calculateXi(rk=ry1)
-    hy2.calculateXi(rk=ry2)
+    hy.calculateXi(rk=ry)
+    xi_hy = hy.xi
+    cov_hy = hy.cov
+    r_hy = np.exp(hy.meanlogr)
 
-    xi_hy1 = hy1.xi
-    xi_hy2 = hy2.xi
-    cov_hy1 = hy1.cov
-    cov_hy2 = hy2.cov
-    r_hy = np.exp(hy1.meanlogr)
+    chi2 = np.dot(xi_hy, np.dot(np.linalg.inv(cov_hy), xi_hy))
 
-    if include_covhy2:
-        cov_tot = cov_hy1 + cov_hy2
-    else:
-        cov_tot = cov_hy1
-    diff_DV = xi_hy1 - xi_hy2
-    chi2 = np.dot(diff_DV, np.dot(np.linalg.inv(cov_tot), diff_DV))
-
-    return chi2, xi_hy1, xi_hy2, cov_hy1, cov_hy2, r_hy
+    return chi2, xi_hy, cov_hy, r_hy
 
 
-def compute_chi2_harmonic_space(inp, y1, y2, h, include_covhy2=True):
+def compute_chi2_harmonic_space(inp, y1, y2, h):
     '''
     ARGUMENTS
     ---------
@@ -137,31 +119,20 @@ def compute_chi2_harmonic_space(inp, y1, y2, h, include_covhy2=True):
     y2: 1D numpy array in RING format containing true y map
         (or y map with inflated CIB)
     h: 1D numpy array in RING format containing halo map
-    include_covhy2: Bool, whether to include covariance of h x y2 in chi2 computation
 
     RETURNS
     -------
     chi2: float, chi^2 value of <h,y1> and <h,y2>
-    hy1: 1D numpy array of length Nbins containing cross-spectrum of halos with y1
-    hy2: 1D numpy array of length Nbins containing cross-spectrum of halos with y2
-    y1y1: 1D numpy array of length Nbins containing auto-spectrum of y1
-    y2y2: 1D numpy array of length Nbins containing auto-spectrum of y2
+    hy: 1D numpy array of length Nbins containing cross-spectrum of halos with (y1-y2)
+    yy: 1D numpy array of length Nbins containing auto-spectrum of (y1-y2)
     hh: 1D numpy array of length Nbins containing auto-spectrum of halos
     '''
-    hy1 = binned(inp, hp.anafast(h, y1, lmax=inp.ellmax))
-    hy2 = binned(inp, hp.anafast(h, y2, lmax=inp.ellmax))
+    hy = binned(inp, hp.anafast(h, y1-y2))
     hh = binned(inp, hp.anafast(h, lmax=inp.ellmax))
-    y1y1 = binned(inp, hp.anafast(y1, lmax=inp.ellmax))
-    y2y2 = binned(inp, hp.anafast(y2, lmax=inp.ellmax))
-    cov_hy1 = cov(inp, np.array([[hh, hy1], [hy1, y1y1]]))
-    cov_hy2 = cov(inp, np.array([[hh, hy2], [hy2, y2y2]]))
-    if include_covhy2:
-        cov_tot = cov_hy1 + cov_hy2
-    else:
-        cov_tot = cov_hy1
-    diff_DV = hy1-hy2
-    chi2 = np.dot(diff_DV, np.dot(np.linalg.inv(cov_tot), diff_DV))
-    return chi2, hy1, hy2, y1y1, y2y2, hh
+    yy = binned(inp, hp.anafast(y1-y2, lmax=inp.ellmax))
+    cov_hy = cov(inp, np.array([[hh, hy], [hy, yy]]))
+    chi2 = np.dot(hy, np.dot(np.linalg.inv(cov_hy), hy))
+    return chi2, hy, yy, hh
 
 
 def compare_chi2(inp, env, beta, ra_halos, dec_halos, h):
@@ -185,13 +156,13 @@ def compare_chi2(inp, env, beta, ra_halos, dec_halos, h):
     y_recon = setup_pyilc(inp, env, beta, inflated=False, suppress_printing=(not inp.debug))
     y_recon_inflated = setup_pyilc(inp, env, beta, inflated=True, suppress_printing=(not inp.debug))
     if inp.harmonic_space:
-        chi2_true, uninflxh, tszxh, uninfl_auto, tsz_auto, h_auto = compute_chi2_harmonic_space(inp, y_recon, y_true, h, include_covhy2=False)
-        chi2_inflated, uninflxh, inflxh, uninfl_auto, infl_auto, h_auto = compute_chi2_harmonic_space(inp, y_recon, y_recon_inflated, h)
-        plot_corr_harmonic(inp, beta, uninflxh, inflxh, tszxh, uninfl_auto, infl_auto, tsz_auto, h_auto)
+        chi2_true, hy_true, yy_true, hh = compute_chi2_harmonic_space(inp, y_recon, y_true, h)
+        chi2_inflated, hy_infl, yy_infl, hh = compute_chi2_harmonic_space(inp, y_recon, y_recon_inflated, h)
+        plot_corr_harmonic(inp, beta, hy_true, hy_infl, yy_true, yy_infl, hh)
     else:   
-        chi2_true, uninflxh, tszxh, cov_hyuninfl, cov_hytrue, r_hy = compute_chi2_real_space(inp, y_recon, y_true, ra_halos, dec_halos, beta, include_covhy2=False)
-        chi2_inflated, uninflxh, inflxh, cov_hyuninfl, cov_hyinfl, r_hy = compute_chi2_real_space(inp, y_recon, y_recon_inflated, ra_halos, dec_halos, beta)
-        plot_corr_real(inp, beta, uninflxh, inflxh, tszxh, cov_hyuninfl, cov_hyinfl, cov_hytrue, r_hy)
+        chi2_true, hy_true, cov_hytrue, r_hy = compute_chi2_real_space(inp, y_recon, y_true, ra_halos, dec_halos, beta, include_covhy2=False)
+        chi2_inflated, hy_infl, cov_hyinfl, r_hy = compute_chi2_real_space(inp, y_recon, y_recon_inflated, ra_halos, dec_halos, beta)
+        plot_corr_real(inp, beta, hy_true, hy_infl, cov_hytrue, cov_hyinfl, r_hy)
     if inp.debug:
         print(f'chi2_true for beta={beta}: {chi2_true}', flush=True)
         print(f'chi2_inflated for beta={beta}: {chi2_inflated}', flush=True)
--- plot_correlations.py
+++ plot_correlations.py
@@ -6,7 +6,7 @@ from scipy import stats
 from utils import binned, cov
 
 
-def plot_corr_harmonic(inp, beta, uninflxh, inflxh, tszxh, uninfl_auto, infl_auto, tsz_auto, h_auto):
+def plot_corr_harmonic(inp, beta, hy_true, hy_infl, yy_true, yy_infl, hh):
     '''
     Plots harmonic space correlations (power spectra) given the spectra
 
@@ -14,26 +14,23 @@ def plot_corr_harmonic(inp, beta, uninflxh, inflxh, tszxh, uninfl_auto, infl_aut
     ---------
     inp: Info object containing input parameter specifications
     beta: float, value of beta for CIB deprojection
-    uninflxh: 1D numpy array of length Nbins containing cross-spectrum of halos with reconstructed y 
-        (without CIB inflation)
-    inflxh: 1D numpy array of length Nbins containing cross-spectrum of halos with reconstructed y 
-        (with CIB inflation)
-    tszxh: 1D numpy array of length Nbins containing cross-spectrum of halos with the true y map
-    uninfl_auto: 1D numpy array of length Nbins containing auto-spectrum of reconstructed y
-        (without CIB inflation)
-    infl_auto: 1D numpy array of length Nbins containing auto-spectrum of reconstructed y
-        (with CIB inflation)
-    tsz_auto: 1D numpy array of length Nbins containing auto-spectrum of the true y map
-    h_auto: 1D numpy array of length Nbins containing auto-spectrum of halo map
+    hy_true: 1D numpy array of length Nbins containing cross-spectrum of halos with 
+        (y_reconstructed - y_true)
+    hy_infl: 1D numpy array of length Nbins containing cross-spectrum of halos with 
+        (y_reconstructed - y_reconstructed_with_inflated_cib)
+    yy_true: 1D numpy array of length Nbins containing auto-spectrum of 
+        (y_reconstructed - y_true)
+    yy_infl: 1D numpy array of length Nbins containing auto-spectrum of 
+        (y_reconstructed - y_reconstructed_with_inflated_cib)
+    hh: 1D numpy array of length Nbins containing auto-spectrum of halo map
 
     RETURNS
     -------
     None (saves plots in {output_dir}/plot_correlations)
     '''
     # compute covariances
-    cov_hytrue = cov(inp, np.array([[tsz_auto, tszxh], [tszxh, h_auto]]))
-    cov_hyuninfl = cov(inp, np.array([[uninfl_auto, uninflxh], [uninflxh, h_auto]]))
-    cov_hyinfl = cov(inp, np.array([[infl_auto, inflxh], [inflxh, h_auto]]))
+    cov_hytrue = cov(inp, np.array([[yy_true, hy_true], [hy_true, hh]]))
+    cov_hyinfl = cov(inp, np.array([[yy_infl, hy_infl], [hy_infl, hh]]))
 
     # plot
     ells = np.arange(inp.ellmax+1)
@@ -42,24 +39,23 @@ def plot_corr_harmonic(inp, beta, uninflxh, inflxh, tszxh, uninfl_auto, infl_aut
     mean_ells = (res[1][:-1]+res[1][1:])/2
     to_dl = mean_ells*(mean_ells+1)/2/np.pi
     plt.clf()
-    plt.errorbar(mean_ells, to_dl*uninflxh, label='y recon.', yerr=to_dl*np.sqrt(np.diagonal(cov_hyuninfl)))
-    plt.errorbar(mean_ells, to_dl*inflxh, label='y recon. (CIB inflated)', yerr=to_dl*np.sqrt(np.diagonal(cov_hyinfl)), linestyle='dashed')
-    plt.errorbar(mean_ells, to_dl*tszxh, label='y true', yerr=to_dl*np.sqrt(np.diagonal(cov_hytrue)), linestyle='dotted')
+    plt.errorbar(mean_ells, to_dl*hy_true, label=r'$C_\ell^{h, (\mathrm{yrecon.-ytrue})}$', yerr=to_dl*np.sqrt(np.diagonal(cov_hytrue)), linestyle='solid')
+    plt.errorbar(mean_ells, to_dl*hy_infl, label=r'$C_\ell^{h, (\mathrm{yrecon.-yrecon_infl_cib})}$', yerr=to_dl*np.sqrt(np.diagonal(cov_hyinfl)), linestyle='dashed')
     plt.grid()
-    plt.ylabel(r'$\ell(\ell+1)/(2\pi)$ [$\mu \mathrm{K}^2$]')
+    plt.ylabel(r'$\ell(\ell+1)C_\ell /(2\pi)$ [$\mu \mathrm{K}^2$]')
     plt.xlabel(r'$\ell$')
     plt.legend()
     plt.title(r'$h \times y$, $\beta=$' + f'{beta:0.3f}')
     plt.savefig(f'{inp.output_dir}/correlation_plots/beta_{beta:0.3f}.png')
 
     # save mean_ells, spectra, and errors to plot later
-    to_save = [mean_ells, to_dl, uninflxh, inflxh, tszxh, uninfl_auto, infl_auto, tsz_auto, h_auto]
+    to_save = [mean_ells, to_dl, hy_true, hy_infl, yy_true, yy_infl, hh]
     pickle.dump(to_save, open(f'{inp.output_dir}/correlation_plots/beta_{beta:0.3f}.p', 'wb'))
 
     return
 
 
-def plot_corr_real(inp, beta, uninflxh, inflxh, tszxh, cov_hyuninfl, cov_hyinfl, cov_hytrue, r_hy):
+def plot_corr_real(inp, beta, hy_true, hy_infl, cov_hytrue, cov_hyinfl, r_hy):
     '''
     Plots real space correlations
 
@@ -67,17 +63,14 @@ def plot_corr_real(inp, beta, uninflxh, inflxh, tszxh, cov_hyuninfl, cov_hyinfl,
     ---------
     inp: Info object containing input parameter specifications
     beta: float, value of beta for CIB deprojection
-    uninflxh: 1D numpy array of length nrad containing correlation of halos with reconstructed y 
-        (without CIB inflation)
-    inflxh: 1D numpy array of length nrad containing correlation of halos with reconstructed y 
-        (with CIB inflation)
-    tszxh: 1D numpy array of length nrad containing correlation of halos with the true y map
-    cov_hyuninfl: 3D numpy array of shape (2,2,nrad) containing covariance 
-        matrix of halos and the reconstructed y map (without CIB inflation)
-    cov_hyinfl: 3D numpy array of shape (2,2,nrad) containing covariance 
-        matrix of halos and the reconstructed y map (with CIB inflation)
-    cov_hytrue: 3D numpy array of shape (2,2,nrad) containing covariance 
-        matrix of halos and the true y map
+    hy_true: 1D numpy array of length nrad containing correlation of halos with 
+        (reconstructed y - true y)
+    hy_infl: 1D numpy array of length nrad containing correlation of halos with 
+        (reconstructed y - reconstructed y with inflated CIB)
+    cov_hytrue: 2D numpy array of shape (nrad, nrad) containing covariance 
+        matrix of halos and (reconstructed y - true y)
+    cov_hyinfl: 2D numpy array of shape (nrad, nrad) containing covariance 
+        matrix of halos and (reconstructed y - reconstructed y with inflated CIB)
     r_hy: 1D numpy array of length nrad containing x-axis point for plotting
 
     RETURNS
@@ -87,16 +80,15 @@ def plot_corr_real(inp, beta, uninflxh, inflxh, tszxh, cov_hyuninfl, cov_hyinfl,
 
     # plot
     plt.clf()
-    plt.errorbar(r_hy, uninflxh, yerr=np.sqrt(np.diag(cov_hyuninfl)), ls='', marker='x', ms=3.0, label='y recon.')
-    plt.errorbar(1.02*r_hy, inflxh, yerr=np.sqrt(np.diag(cov_hyinfl)), ls='', marker='o', ms=3.0, label='y recon. (CIB inflated)')
-    plt.errorbar(1.04*r_hy, tszxh, yerr=np.sqrt(np.diag(cov_hytrue)), ls='', marker='v', ms=3.0, label='y true')
+    plt.errorbar(r_hy, hy_true, yerr=np.sqrt(np.diag(cov_hytrue)), ls='', marker='v', ms=3.0, label='h x (y recon. - y true)')
+    plt.errorbar(1.02*r_hy, hy_infl, yerr=np.sqrt(np.diag(cov_hyinfl)), ls='', marker='o', ms=3.0, label='h x (y recon. - y recon. (CIB inflated))')
     plt.legend()
     plt.xscale('log')
     plt.yscale('log')
     plt.savefig(f'{inp.output_dir}/correlation_plots/beta_{beta:0.3f}.png')
 
-    # save mean_ells, spectra, and errors to plot later
-    to_save = [uninflxh, inflxh, tszxh, cov_hyuninfl, cov_hyinfl, cov_hytrue]
+    # save spectra and errors to plot later
+    to_save = [hy_true, hy_infl, cov_hytrue, cov_hyinfl, r_hy]
     pickle.dump(to_save, open(f'{inp.output_dir}/correlation_plots/beta_{beta:0.3f}.p', 'wb'))
 
     return
@@ -123,19 +115,13 @@ def plot_corr_harmonic_from_map(inp, beta, y_true, y_recon, y_recon_inflated, h)
     '''
 
     # compute all the spectra
-    tsz_auto = binned(inp, hp.anafast(y_true, lmax=inp.ellmax))
-    uninfl_auto = binned(inp, hp.anafast(y_recon, lmax=inp.ellmax))
-    infl_auto = binned(inp, hp.anafast(y_recon_inflated, lmax=inp.ellmax))
-    h_auto = binned(inp, hp.anafast(h, lmax=inp.ellmax))
-    uninflxh = binned(inp, hp.anafast(y_recon, h, lmax=inp.ellmax))
-    inflxh = binned(inp, hp.anafast(y_recon_inflated, h, lmax=inp.ellmax))
-    tszxh = binned(inp, hp.anafast(y_true, h, lmax=inp.ellmax))
-    uninfl_auto = binned(inp, hp.anafast(y_recon, lmax=inp.ellmax))
-    infl_auto = binned(inp, hp.anafast(y_recon_inflated, lmax=inp.ellmax))
-    tsz_auto = binned(inp, hp.anafast(y_true, lmax=inp.ellmax))
-    h_auto = binned(inp, hp.anafast(h, lmax=inp.ellmax))
+    hy_true = binned(inp, hp.anafast(h, y_recon-y_true, lmax=inp.ellmax))
+    hy_infl = binned(inp, hp.anafast(h, y_recon-y_recon_inflated, lmax=inp.ellmax))
+    yy_true = binned(inp, hp.anafast(y_recon-y_true, lmax=inp.ellmax))
+    yy_infl = binned(inp, hp.anafast(y_recon-y_recon_inflated, lmax=inp.ellmax))
+    hh = binned(inp, hp.anafast(h, lmax=inp.ellmax))
     
     # plot and save
-    plot_corr_harmonic(inp, beta, uninflxh, inflxh, tszxh, uninfl_auto, infl_auto, tsz_auto, h_auto)
+    plot_corr_harmonic(inp, beta, hy_true, hy_infl, yy_true, yy_infl, hh)
     
     return
